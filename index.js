64b53b0c592c05f6459255070ca86a4cb6a149e2
Kitadi Elie <elk.dev.official@gmail.com>
2022-04-09 02:02:23 +0300

    1. Switched to sass 2. Added Dots Navigation

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+// Sidebar Navigation
 const menu_toggle = document.querySelector(".menu_toggle");
 const navigation = document.querySelector(".navigation");
 
 menu_toggle.addEventListener("click", () => {
   navigation.classList.toggle("active");
+});
+
+//Dots Navigation
+
+// const menu_toggle = document.querySelector(".menu_toggle");
+const dots_navigation = document.querySelector(".dots_navigation");
+
+dots_navigation.addEventListener("click", () => {
+  dots_navigation.classList.toggle("active");
 });
