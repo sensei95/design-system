7ec0db74246e8bde4a4e402f1acf9b6f68bb7091
Kitadi Elie <elk.dev.official@gmail.com>
2022-04-09 03:31:34 +0300

    1. Switched to sass 2. Added Circular Navigation

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ menu_toggle.addEventListener("click", () => {
 });
 
 //Dots Navigation
-
-// const menu_toggle = document.querySelector(".menu_toggle");
 const dots_navigation = document.querySelector(".dots_navigation");
 
 dots_navigation.addEventListener("click", () => {
   dots_navigation.classList.toggle("active");
 });
+
+//Circular Navigation
+const circular_menu = document.querySelector(".circular_menu");
+const toggle = document.querySelector(".toggle");
+const circular_menu_items = circular_menu.querySelectorAll("li");
+
+toggle.addEventListener("click", () => {
+  circular_menu.classList.toggle("active");
+  toggle.classList.toggle("active");
+});
+
+function activeCircularMenuItem(e) {
+  e.preventDefault();
+
+  circular_menu_items.forEach((item) => {
+    item.classList.remove("active");
+    this.classList.add("active");
+  });
+}
+
+circular_menu_items.forEach((item) =>
+  item.addEventListener("click", activeCircularMenuItem)
+);
